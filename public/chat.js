039243cdcf076b94ddcbdfbf86b94b3269a8e24d
Rustin Wollin <rwollin96@gmail.com>
2014-01-21 17:05:26 -0500

    attempt socket.io connection on location.host

--- public/chat.js
+++ public/chat.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	var messages = [];
-	var socket = io.connect('http://localhost:8080');
+	var socket = io.connect('http://'+location.host);
 	var field = document.getElementById("field");
 	var sendButton = document.getElementById("send");
 	var content = document.getElementById("content");
