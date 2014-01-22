3c8dfed766017c27f2d8a1287224c8a416f73dd3
Rustin Wollin <rwollin96@gmail.com>
2014-01-21 21:47:34 -0500

    allow environment to set port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var port = 8080;
+var port = process.env.PORT || 8080;
 
 app.set('views', __dirname + '/views');
 app.set('view engine', "jade");
