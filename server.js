a017eb75db141152a1db228b841ad754985bd2ff
Rustin Wollin <rwollin96@gmail.com>
2014-01-22 18:24:22 -0500

    assign hard coded roles to players

--- server.js
+++ server.js
@@ -14,12 +14,58 @@ app.use(express.static(__dirname + '/public'));
 var io = require('socket.io').listen(app.listen(port));
 console.log("Listening on port " + port);
 
+gameState = 0; //0: not yet started, 1: running
+
+playerRoles = [
+	{role: 'villager', group: 'village'},
+	{role: 'villager', group: 'village'},
+	{role: 'villager', group: 'village'},
+	{role: 'cop', group: 'village'},
+	{role: 'doctor', group: 'village'},
+	{role: 'mafioso', group: 'mafia'},
+	{role: 'mafioso', group: 'mafia'}
+];
+
+function shuffle(array) {
+	var m = array.length, t, i;
+
+	while (m) {
+		i = Math.floor(Math.random() * m--);
+
+		t = array[m];
+		array[m] = array[i];
+		array[i] = t;
+	}
+
+	return array;
+}
+
+function assignRoles() {
+	var players = [];
+	io.sockets.clients().forEach(function (socket) {
+		players.push(socket);
+	});
+	players = shuffle(players);
+
+	for (var i = 0; i < players.length; i++) {
+		if (i <= playerRoles.length - 1) {
+			players[i].join(playerRoles[i].role);
+			players[i].join(playerRoles[i].group);
+			players[i].emit('message', { message: 'You have been assigned the role of ' + playerRoles[i].role + '. You are affiliated with the ' + playerRoles[i].group + '.' });
+		} else {
+			players[i].join('spectator');
+			players[i].emit('message', { message: 'Since the roles are full, you have been assigned the role of spectator.' });
+		}
+	}
+}
+
 function checkNumPlayers() {
 	var clients = io.sockets.clients().length;
-	var reqPlayers = 7;
+	var reqPlayers = playerRoles.length;
 	if(clients >= reqPlayers) {
 		io.sockets.emit('announcement', { message: 'Required number of players reached'});
-		//start the game loop here
+		assignRoles();
+		gameState = 1;
 	} else {
 		io.sockets.emit('announcement', { message: 'Waiting on ' + (reqPlayers - clients) + ' more players'});
 	}
@@ -29,14 +75,18 @@ io.sockets.on('connection', function (socket) {
 	socket.emit('message', { message: 'Welcome to the lobby.' });
 	io.sockets.emit('message', { message: 'A new client has connected.' });
 
-	checkNumPlayers();
+	if(!gameState){
+		checkNumPlayers();
+	}
 
 	socket.on('disconnect', function() {
 		io.sockets.emit('message', { message: 'A client has disconnected.' });
-		setTimeout(function() {
-			checkNumPlayers();
-		}, 1000);
-	})
+		if(!gameState){
+			setTimeout(function() {
+				checkNumPlayers();
+			}, 1000);
+		}
+	});
 
 	socket.on('send', function (data) {
 		io.sockets.emit('message', data);
