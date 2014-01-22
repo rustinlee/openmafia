4d3bdc05b4ce9dfa45d643e24693af3e20a563af
Rustin Wollin <rwollin96@gmail.com>
2014-01-21 23:03:41 -0500

    added new div for server announcements

--- public/chat.js
+++ public/chat.js
@@ -22,6 +22,10 @@ $(document).ready(function() {
 		}
 	});
 
+	socket.on('announcement', function (data) {
+		announcement.innerHTML = '<h1>' + data.message + '</h1>';
+	})
+
     $("#field").keyup(function(e) {
         if(e.keyCode == 13) {
             sendMessage();
