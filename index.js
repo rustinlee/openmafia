2c08ce88485f1ce56965346ffec9be0a0ef6cb9a
Rustin Wollin <rwollin96@gmail.com>
2014-01-21 23:06:14 -0500

    server counts clients on connect & disconnect

--- index.js
+++ index.js
@@ -14,8 +14,30 @@ app.use(express.static(__dirname + '/public'));
 var io = require('socket.io').listen(app.listen(port));
 console.log("Listening on port " + port);
 
+function checkNumPlayers() {
+	var clients = io.sockets.clients().length;
+	var reqPlayers = 7;
+	if(clients >= reqPlayers) {
+		io.sockets.emit('announcement', { message: 'Required number of players reached'});
+		//start the game loop here
+	} else {
+		io.sockets.emit('announcement', { message: 'Waiting on ' + (reqPlayers - clients) + ' more players'});
+	}
+}
+
 io.sockets.on('connection', function (socket) {
-	socket.emit('message', { message: 'welcome to the chat' });
+	socket.emit('message', { message: 'Welcome to the lobby.' });
+	io.sockets.emit('message', { message: 'A new client has connected.' });
+
+	checkNumPlayers();
+
+	socket.on('disconnect', function() {
+		io.sockets.emit('message', { message: 'A client has disconnected.' });
+		setTimeout(function() {
+			checkNumPlayers();
+		}, 1000);
+	})
+
 	socket.on('send', function (data) {
 		io.sockets.emit('message', data);
 	});
